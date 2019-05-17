4d0b704b2da350f56a446d79ead048e8adff1cc1
Sara Wegmann <sarawegmann@Saras-MacBook-Air.local>
2019-05-17 14:27:15 -0400

    completed sessons and users controller, added controllers to server.js

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+const session = require('express-session');
+
 
 app.get('/', (req,res) => {
     res.send('hello world');
 });
+
+const userController = require('./controllers/users.js');
+app.use('/users', userController);
+const sessionsController = require('./controllers/sessions.js');
+app.use('/sessions', sessionsController);
 
 app.listen(3000, () => {
     console.log('listening on port 3000...');
